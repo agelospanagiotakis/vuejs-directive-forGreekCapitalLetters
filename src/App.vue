d37fbca934f312cd6d624150649b924faa1d3a4a
agelospanagiotakis <agelospanagiotakis@gmail.com>
2017-06-11 20:24:11 +0300

    adding progress bar

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,46 @@
 <template>
   <div id="app">
+                    <asidebar :show="showSideMenu" @close="showSideMenu = false"   placement="left" header="Μενού" :width="350">
+                            <h4> Επιλέξτε τύπο παιγνιδιού : </h4>
+                             <div class="btn-group btn-group-vertical ">
+                                   <a class="btn btn-lg btn-default" :class="{ 'btn-primary' : activePraxis == '+' }"  @click="setPraxis(this,'+')">Πρόσθεση</a><br />
+                                   <a class="btn btn-lg btn-default" :class="{ 'btn-primary' : activePraxis == '-' }"   @click="setPraxis(this,'-')">Αφαίρεση</a><br />
+                                   <a class="btn btn-lg btn-default" :class="{ 'btn-primary' : activePraxis == '*' }"   @click="setPraxis(this,'*')">Πολλαπλασιασμός</a><br />
+                                   <a class="btn btn-lg btn-default" :class="{ 'btn-primary' : activePraxis == '/' }"   @click="setPraxis(this,'/')">Διαίρεση</a><br />
+                              </div>
+                      </asidebar>
+                     <asidebar   :show="showSideMenu_Options" @close="showSideMenu_Options = false"  placement="right" header="Επιλογές" :width="350">
+                                <h4> Ρύθμιση δυσκολίας: </h4>
+                                    μικρότερος αριθμός: 0 <br />
+                                    μέγιστος αριθμός:100 <br />
+                      </asidebar>
+                      <div class="progress">
+                        <progressbar :now="countAnswersPercentage" type="primary"></progressbar>
+                      </div>
 
-  <asidebar :show="showSideMenu" @close="showSideMenu = false"   placement="left" header="Μενού" :width="350">
-          <h4> Επιλέξτε τύπο παιγνιδιού : </h4>
-           <div class="btn-group btn-group-vertical ">
-                 <a class="btn btn-lg btn-default" :class="{ 'btn-primary' : activePraxis == '+' }"  @click="setPraxis(this,'+')">Πρόσθεση</a><br />
-                 <a class="btn btn-lg btn-default" :class="{ 'btn-primary' : activePraxis == '-' }"   @click="setPraxis(this,'-')">Αφαίρεση</a><br />
-                 <a class="btn btn-lg btn-default" :class="{ 'btn-primary' : activePraxis == '*' }"   @click="setPraxis(this,'*')">Πολλαπλασιασμός</a><br />
-                 <a class="btn btn-lg btn-default" :class="{ 'btn-primary' : activePraxis == '/' }"   @click="setPraxis(this,'/')">Διαίρεση</a><br />
-            </div>
-    </asidebar>
-   <asidebar   :show="showSideMenu_Options" @close="showSideMenu_Options = false"  placement="right" header="Επιλογές" :width="350">
-              <h4> Ρύθμιση δυσκολίας: </h4>
-                  μικρότερος αριθμός: 0 <br />
-                  μέγιστος αριθμός:100 <br />
-    </asidebar>
+                      <div class="pull-right">
+                            <div class="totalScore pull-right">
+                              Σκόρ : <span class="use-digital-font"> {{totalScore}} </span>
+                            </div>
 
-    <div class="pull-right">
-      <div class="totalScore pull-right">
-        Σκόρ : <span class="use-digital-font"> {{totalScore}} </span>
-      </div>
-        <div v-show="countAnswers > 0" class="ScoreCountedAnswers pull-right">
-          {{countCorrectAnswers}} σωστές στις  {{countAnswers}}
-        </div>
-        <div class="btnActions">
-          <button  class="btn btn-danger"  v-on:click="debugAction">Debug</button>
-          <button class="btn btn-success"  @click="showMenu">Μενού</button>
-          <button class="btn btn-warning"  @click="showOptions">Ρυθμίσεις</button>
-    </div>
-  </div>
-     <h4>Τεστ Ανθρώπινης Αριθμομηχανής</h4>
-      <div class="panel panel-success">
-        <h2>{{typeofpraxisStr}}</h2>
-        <praxis :typeofpraxis="activePraxis" @answerGiven="answered($event)"></praxis>
-      </div>
-<!--
-    <alert :show.sync="showModal" placement="top-right"   type="success" width="400px" dismissable>
-      <span class="icon-ok-circled alert-icon-float-left"></span>
-      <strong>{{ModalMsg}}</strong>
-      <p></p>
-    </alert>
--->
+
+                            <div v-show="countAnswers > 0" class="ScoreCountedAnswers pull-right">
+                                        {{countCorrectAnswers}} σωστές στις  {{countAnswers}}
+
+                            </div>
+
+                          <div class="btnActions">
+                            <button  class="btn btn-danger"  v-on:click="debugAction">Debug</button>
+                            <button class="btn btn-success"  @click="showMenu">Μενού</button>
+                            <button class="btn btn-warning"  @click="showOptions">Ρυθμίσεις</button>
+                      </div>
+                    </div>
+                       <h4>Τεστ Ανθρώπινης Αριθμομηχανής</h4>
+                        <div class="panel panel-success">
+                          <h2>{{typeofpraxisStr}}</h2>
+                          <praxis :typeofpraxis="activePraxis" @answerGiven="answered($event)"></praxis>
+                        </div>
+
   </div>
 </template>
 
@@ -49,13 +49,15 @@ import praxis from './components/praxis.vue'
 //var alert = require('vue-strap/src/alert');
 var alert = require('vue-strap').alert;
 var asidebar = require('vue-strap').aside;
+var progressbar = require('vue-strap').progressbar;
 
 export default {
   name: 'app',
   components: {
       praxis,
       alert,
-      asidebar
+      asidebar,
+      progressbar
     },
     data () {
     return {
@@ -63,6 +65,8 @@ export default {
       activePraxis: '+',
       totalScore: 0,
       countAnswers : 0,
+      totalAnswersToFinish:5,
+      countAnswersPercentage : 0 ,
       countCorrectAnswers : 0,
       scoreChangeIncrement : 10,
       scoreChangeDecrement : 1,
@@ -72,6 +76,9 @@ export default {
       showSideMenu_Options: false
     }
   },
+  computed:{
+
+    },
   // define methods under the `methods` object
   methods: {
     answered: function (isCorrect) {
@@ -84,8 +91,11 @@ export default {
         this.totalScore -= this.scoreChangeDecrement;
       }
 
+      let perc =  (this.countAnswers / this.totalAnswersToFinish) *  100;
+      console.log("perc",perc);
+      this.countAnswersPercentage =  perc;
 
-    },
+  },
     showMenu(){
       this.showSideMenu =  true;
     },
@@ -100,7 +110,6 @@ export default {
       if ( praxis == "-") this.typeofpraxisStr = "Αφαίρεση";
       if ( praxis == "/") this.typeofpraxisStr = "Διαίρεση";
       if ( praxis == "*") this.typeofpraxisStr = "Πολλαπλασιασμός";
-
     },
     debugAction: function (event) {
        console.log('debug here',event );
